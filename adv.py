a9ac909829145cb29a611d88dcfee0c0300409ad
squashgray <squashgray@gmail.com>
2020-05-19 19:51:27 -0700

    mvp

--- adv.py
+++ adv.py
@@ -25,10 +25,95 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+ 
 
+s = Stack()
+s.push(player.current_room.id) 
+
+
+
+visited = dict() 
+path = [] 
+reverse = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+
+
+
+visited[player.current_room.id] = player.current_room.get_exits()  
+
+
+
+while len(visited) < len(room_graph):
+    if player.current_room.id not in visited:
+        visited[player.current_room.id] = player.current_room.get_exits()
+        current_move = path[-1]
+        visited[player.current_room.id].remove(current_move)
+    if len(visited[player.current_room.id]) == 0:
+        current_move = path[-1]
+        path.pop()
+        traversal_path.append(current_move)
+        player.travel(current_move)
+    else:
+        next_travel = visited[player.current_room.id][-1]
+        visited[player.current_room.id].pop() 
+        player.travel(next_travel) 
+        path.append(reverse[next_travel])
+        traversal_path.append(next_travel)
+
+
+
+
+# while len(visited) < len(room_graph) - 1: 
+#     v = s.pop()
+#     print("stack",s.stack)
+#     print("v", visited)
+#     if v not in visited:
+#         print("ghgh",v)
+#         visited.add(v) 
+#         for direction in player.current_room.get_exits(): 
+#             player.travel(direction)
+#             traversal_path.append(direction)
+#             direction = player.current_room.id
+#             visited.add(direction) 
+#             print("d", direction)
+#             s.push(direction) 
+
+
+# entries[player.current_room.id] = room_graph[player.current_room.id][1]
+# entries[player.current_room.id] = { i: '?' for i in player.current_room.get_exits() }
+
+# while len(entries) <  2:
+#     new_stack = stack.pop()
+#     prev_room = player.current_room.id
+#     if new_stack not in entries:
+#         entries[new_stack] = { i: '?' for i in player.current_room.get_exits() }
+#         for direction in player.current_room.get_exits():
+#             print('s', stack)
+#             print('d', direction)
+#             player.travel(direction)
+#             print(f'this is where im at {player.current_room.id}')
+#             traversal_path.append(direction)
+#             entries[new_stack][direction] = player.current_room.id
+#             # print(entries[new_stack])
+#             stack.append(player.current_room.id)
+#             # print(entries)
+#             print(traversal_path)
+        
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
